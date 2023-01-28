21662dcb71b7e404aa45afe6c179404a4ce4875d
LHC <lautarohconti@gmail.com>
2023-01-27 22:53:17 -0300

    autenticacion

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from "express";
 import { loginUser, registerNewUser } from "../services/auth";
+import { handleHttp } from "../utils/error.handle";
 
 const registerCtrl = async ({body}: Request , res: Response) => {
-    const response = await registerNewUser(body)
+    try {
+        const newUser = await registerNewUser(body)
+        const response = newUser ? {code: 200, content: newUser} : {code: 400, content: 'Something went wrong'}
+        res.status(response.code).json(response.content)
+    } catch (error:any) {
+        handleHttp(res, 'ERROR_REGISTER_USER', error)
+    }
 }
 
-const loginCtrl = async (req: Request , res: Response) => {
-    const response = await loginUser()
+const loginCtrl = async ({body}: Request , res: Response) => {
+    
+    try {
+        const user = await loginUser(body)
+        const response = user ? {code: 200, content: user} : {code: 403, content: 'Something went wrong'}
+        res.status(response.code).json(response.content)
+    } catch (error:any) {
+        handleHttp(res, 'ERROR_LOGIN_USER', error)
+    }
 }
 
 export {registerCtrl,loginCtrl}
